c69114382028e9f0435f3b8aab78e52a7d2b4752
sahithdaddla <daddlasahith>
2025-06-18 11:03:18 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3007;
 
 // Middleware
 app.use(cors());
@@ -11,9 +11,9 @@ app.use(express.json());
 // PostgreSQL connection configuration
 const pool = new Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: 'postgres',
     database: 'attendance_db',
-    password: 'root',
+    password: 'admin123',
     port: 5432,
 });
 
@@ -203,5 +203,5 @@ app.delete('/api/admin/records', async (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://13.60.22.32:${port}`);
 });
