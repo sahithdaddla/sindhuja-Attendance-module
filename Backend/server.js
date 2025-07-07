edcf05a61add2b1404e70a4d204a8f61f1e24125
sahithdaddla <daddlasahith>
2025-07-07 18:46:39 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3007;
+const port = 3208;
 
 // Middleware
 app.use(cors());
@@ -203,5 +203,5 @@ app.delete('/api/admin/records', async (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running on http://13.53.49.44:${port}`);
+    console.log(`Server running on http://3.85.61.23:${port}`);
 });
