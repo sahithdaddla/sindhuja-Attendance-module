34038199eadc25bc1fd155a29ea10971ec826856
sahithdaddla <daddlasahith>
2025-07-14 12:02:19 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -203,5 +203,5 @@ app.delete('/api/admin/records', async (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running on http://3.85.61.23:${port}`);
+    console.log(`Server running on http://13.51.205.49:${port}`);
 });
